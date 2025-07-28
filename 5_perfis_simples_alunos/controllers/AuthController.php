b1f44472a4c6b405cbc04c1fcd98b7654b4e9535
StefanyAraujoo <stefany.n.araujo@aluno.senai.br>
2025-07-28 10:49:06 -0300

    Concluido

--- 5_perfis_simples_alunos/controllers/AuthController.php
+++ 5_perfis_simples_alunos/controllers/AuthController.php
@@ -1,2 +1,43 @@
-<!-- 2ª Digitação (Aqui)
-Este arquivo será o responsável por: (Controlar a autenticação) -->
+<?php
+/**
+ * Controlador de autenticação
+ */
+class AuthController {
+    /**
+    * Exibe a página de login
+    */
+    public function login($erro = '') {
+        require_once 'views/login.php';
+        renderizarLogin($erro);
+    }
+
+    /**
+     * Processa a tentativa de login
+     */
+    public function autenticar() {
+        $usuario = $_POST['usuario'] ?? '';
+        $senha = $_POST['senha'] ?? '';
+        
+        if (empty($usuario) || empty($senha)) {
+            $this->login('Preencha todos os campos');
+            return;
+        }
+        $dadosUsuario = Auth::autenticar($usuario, $senha);
+        if ($dadosUsuario) {
+            Auth::iniciarSessao($dadosUsuario);
+            header('Location: index.php?pagina=lista');
+            exit;
+        } else {
+            $this->login('Usuário ou senha incorrectos');
+        }
+    }
+    
+    /**
+    * Processa o logout
+    */
+    public function logout() {
+        Auth::encerrarSessao();
+        header('Location: index.php?pagina=login');
+        exit;
+    }
+}
